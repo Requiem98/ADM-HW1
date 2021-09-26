530e572d957055bc2934ae58e87829a7faf659f6
Requiem98 <amedeo.ranaldi@gmail.com>
2021-09-26 17:05:43 +0200

    aggiornato

--- scripts.py
+++ scripts.py
@@ -147,19 +147,75 @@ if __name__ == '__main__':
     for x in range(len(clas)):
         if clas[x][1] == outG:
             print(clas[x][0])
+
+#Finding the percentage
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    student_marks = {}
+    for _ in range(n):
+        line = raw_input().split()
+        name, scores = line[0], line[1:]
+        scores = map(float, scores)
+        student_marks[name] = scores
+    query_name = raw_input()
     
+    out = 0
+    for x in range(3):
+        out = out + student_marks[query_name][x]
+    out = out/3
+    print("%.2f" %out)
+
+#Lists
+
+if __name__ == '__main__':
+    N = int(raw_input())
     
+    lista = []
+    out = ""
+    end = ")"
+    for _ in range(N):
+        inp = raw_input()
+        l = inp.split(" ")
+        if l[0] != "print":
+            out = "lista."+l[0]+"("
+            for x in range(1,len(l)):
+                out = out+l[x]+","
+            if out.endswith(","):
+                out = out[:-1]
+            out = out + end
+            eval(out)
+        else:
+            print(lista)
 
-#
+#Tuples
 
-#
+if __name__ == '__main__':
+    n = int(raw_input())
+    integer_list = map(int, raw_input().split())
+    t = tuple(integer_list)
+    print(hash(t))
 
-#
+#sWAP cASE
 
-#
+def swap_case(s):
+    s = s.swapcase()
+    return s
 
+if __name__ == '__main__':
+    s = raw_input()
+    result = swap_case(s)
+    print result
 
+#String Split and Join
 
+def split_and_join(line):
+    return line.replace(" ","-")
+
+if __name__ == '__main__':
+    line = raw_input()
+    result = split_and_join(line)
+    print result
 
 
 
